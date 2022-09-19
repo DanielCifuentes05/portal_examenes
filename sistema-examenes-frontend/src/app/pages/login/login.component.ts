e37d153adffadc3d8ed2f4a36bbd169feba9e0de
Daniel Cifuentes <d.cifuentes0599@gmail.com>
2022-09-18 21:47:59 -0500

    Se crea servicio login en frontend y Interceptor

--- sistema-examenes-frontend/src/app/pages/login/login.component.ts
+++ sistema-examenes-frontend/src/app/pages/login/login.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  logInData = {
+    username: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor(private snack: MatSnackBar, private loginService: LoginService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  formSubmit() {
+    if (
+      this.logInData.username.trim() == '' ||
+      this.logInData.username.trim() == null
+    ) {
+      this.snack.open('El nombre de usuario es requerido!!', 'Aceptar', {
+        duration: 3000,
+      });
+      return;
+    }
+
+    if (
+      this.logInData.password.trim() == '' ||
+      this.logInData.password.trim() == null
+    ) {
+      this.snack.open('La Contraseña es requerida!!', 'Aceptar', {
+        duration: 3000,
+      });
+      return;
+    }
+
+    this.loginService.generateToken(this.logInData).subscribe(
+      (data: any) => {
+        console.log(data);
+        this.loginService.loginUser(data.token);
+        this.loginService.getCurrentUser().subscribe((user: any) => {
+          console.log(user);
+        });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
